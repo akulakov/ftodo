c3ee4d30a2e758a5238c86ecad27def2091f3c67
AK <andrei.avk@gmail.com>
2016-11-03 22:01:37 -0400

    refactor of cmd process method; changed folder related commands; updated all command arg handling

--- ftodo.py
+++ ftodo.py
@@ -3,10 +3,15 @@
 from datetime import datetime, timedelta
 import shelve, math
 import os, sys
+import re
 
 itemdb="/Users/ak/Dropbox/etodo/todoitems"
 # itemdb="todoitems.db"
 itemdb = "todoitems"
+
+"""
+status codes: 1 - normal; 2 - done
+"""
 
 class Item:
     def __init__(self, n):
@@ -18,6 +23,8 @@ class Item:
 
     def __repr__(self):
         expires_in = self.expire - datetime.now()
+
+        # show time when less than 12 hours left
         if self.status == 1 and 0 < expires_in.total_seconds() < 12*3600:
             return self.with_time()
         return self.name
@@ -26,7 +33,7 @@ class Item:
         def y(a): return str(int(math.floor(a)))
         x = self.expire-datetime.now()
         m = x.total_seconds()/60
-        h,m=m//60,m%60
+        h,m = m//60,m%60
         return self.name +' '+y(h)+':'+y(m)#+' exp: '+str(self.expire)
 
 
@@ -36,8 +43,12 @@ class Items:
         items = s.get('items') or []
         self.folders = s.get('folders')
         if not self.folders:
-            self.folders = dict(a=items)
-        self.current_folder = s.get('current_folder') or 'a'
+            self.folders = dict(main=items)
+        if 'a' in self.folders and not 'main' in self.folders:
+            self.folders['main'] = self.folders["a"]
+        self.current_folder = s.get('current_folder') or 'main'
+        if self.current_folder=='a':
+            self.current_folder = 'main'
         self.items = self.folders[self.current_folder]
 
     def add_folder(self, name):
@@ -47,13 +58,25 @@ class Items:
         else:
             print('"%s" folder already exists' % name)
 
+    def delete_folder(self, name):
+        if self.current_folder == name:
+            print("Error: cannot remove active (current) folder.")
+        elif name not in self.folders:
+            print("Error: no such folder!")
+        else:
+            del self.folders[name]
+
     def set_current_folder(self, name):
-        if name in self.folders:
+        flds = [f for f in self.folders.keys() if f.lower().startswith(name.lower())]
+        if len(flds)>1:
+            print("More than one match: %s"%', '.join(flds))
+        elif not flds:
+            print("Folder not found; available folders: %s" % ', '.join(self.folders.keys()))
+        else:
+            name = flds[0]
             self.folders[self.current_folder] = self.items
             self.items = self.folders[name]
             self.current_folder = name
-        else:
-            print('Folder not found; available folders: %s' % ' '.join(self.folders.keys()))
 
     def save(self):
         s = shelve.open(itemdb)
@@ -79,7 +102,7 @@ class Items:
 
     def faded(self):
         """These will disappear within 24hr."""
-        return [i for i in self.items if i.fade < datetime.now() < i.expire and i.status ==1]
+        return [i for i in self.items if i.fade < datetime.now() < i.expire and i.status==1]
 
     def expired(self):
         return [i for i in self.items if datetime.now() >= i.expire and i.status==1]
@@ -100,7 +123,10 @@ class ETodo:
                     if i is None:
                         print()
                     else:
-                        print("{0}) {1}".format(n,i))
+                        if len(self.items_)<11:
+                            print("{0}) {1}".format(n,i))
+                        else:
+                            print("{0:>2}) {1}".format(n,i))
                         n+=1
             self.skip_listing=0
 
@@ -118,111 +144,149 @@ class ETodo:
         sys.exit()
 
     def cmd(self, i):
-        cmds = dict(a='add',R='delete',d='done',f='fade',e='expire',b='bump',E='expired',h='help',D='list_done', EE='empty_expired', r='rename',
-                    AF='add_folder', o='folder', lf='list_folders')
-        num=None
-        if len(i)>1:
-            try:
-                num = int(i[1:].strip())-1
-            except:
-                pass
-        try:
-            if i=='EE':
-                self.empty_expired()
-            elif i=='lf':
-                self.list_folders()
-            elif i.startswith('AF'):
-                self.add_folder(i[2:].strip()[0])
-            elif num is not None:
-                getattr(self, cmds[i[0]])(num)
-            else:
-                getattr(self, cmds[i[0]])(i[1:])
-        except Exception as e:
-            print("error:",e)
-            raise e
-            pass
+        i = i.strip()
+        cmds = dict(a='add', R='delete', d='done', f='fade', e='expire', b='bump', E='expired', h='help', D='list_done', r='rename', o='folder',
+                    # double letter commands
+                    EE='empty_expired', AF='add_folder', lf='list_folders', RF='delete_folder',
+                    )
 
-    def help(self,i):
+        # these commands should skip listing current items because user needs to see their output
+        # (i.e. not to have it scrolled up and hidden)
+        skip_listing = "h D EE AF lf RF".split()
+
+        single_match = re.match('^(\D)$', i)
+        # n+ number index arguments
+        num_arg_match = re.match('^(\D+) ?([\d ]+)$', i)
+        # with optional string arg
+        arg_match = re.match('^([\w]+) ?(.+)?$', i)
+        arg = None
+        if single_match:
+            cmd = single_match.groups()[0]
+
+        if single_match and cmd in cmds:
+            pass
+        elif num_arg_match:
+            cmd, arg = num_arg_match.groups()
+            arg = [int(x) for x in arg.strip().split()]
+        elif arg_match:
+            g = list(arg_match.groups())
+            cmd = g.pop(0)
+            if g:
+                arg = g.pop()
+
+        if cmd in cmds:
+            try:
+                getattr(self, cmds[cmd])(arg)
+                if cmd in skip_listing:
+                    self.skip_listing = True
+            except Exception as e:
+                print("ERROR: %s"%e)
+        else:
+            print("ERROR: Command not found: %s"%cmd)
+
+    def help(self, arg):
         print("""
           a)dd
           R)remove
           d)one
           f)ade
-          fo)lder
+          f o)lder
           e)xpire
           b)ump
           E)xpired list
           EE) empty expired
           AF) add folder
+          RF) remove folder
           lf) list folders
           D)one list
           r)ename
           h)elp""")
-        self.skip_listing=1
 
-    def list_folders(self):
+    def list_folders(self, arg):
         print()
         print(' '.join(items.folders.keys()))
         print('\n'*3)
 
-    def list_done(self, i):
+    def list_done(self, arg):
         it = items.done()
         for n,i in enumerate(it):
-            print("{0}) {1}".format(n+1,i))
+            print(i)
 
     def add(self, name):
         items.add(Item(name.strip()))
         print('\n'*3)
 
-    def delete(self, i):
-        it = list(filter(None, self.items_))
-        x= it[i]
-        # print('removing ',x)
-        items.items.remove(x)
-        # print("items.items", items.items)
+    def delete(self, indexes):
+        for i in reversed(sorted(indexes)):
+            i = int(i)-1
+            it = list(filter(None, self.items_))
+            x= it[i]
+            # print('removing ',x)
+            items.items.remove(x)
+            # print("items.items", items.items)
 
-    def done(self, i):
-        self.items_[i].status=2
+    def done(self, indexes):
+        for i in indexes:
+            i = int(i)-1
+            i = list(filter(None, self.items_))[i]
+            self.items_[i].status=2
 
-    def rename(self, i):
-        print('Rename')
-        it = list(filter(None, self.items_))
-        x = it[i]
-        print(x.name)
-        new = input('New > ')
-        if new.strip():
-            x.name = new.strip()
-        print('\n'*3)
+    def rename(self, indexes):
+        for i in indexes:
+            i = int(i)-1
+            print('Rename')
+            it = list(filter(None, self.items_))
+            x = it[i]
+            print(x.name)
+            new = input('New > ')
+            if new.strip():
+                x.name = new.strip()
+            print('\n'*3)
 
 
     def add_folder(self, name):
         items.add_folder(name)
 
+    def delete_folder(self, name):
+        i = input("WARNING! Are you sure you wish to remove folder '%s' [Y/N]? " % name)
+        if i=='Y':
+            items.delete_folder(name)
+            print("Removed folder '%s'" % name)
+
     def folder(self, name):
         items.set_current_folder(name)
 
-    def fade(self, i):
-        self.items_[i].fade = datetime.now()
-        self.items_[i].expire = datetime.now()+timedelta(hours=24)
+    def fade(self, indexes):
+        for i in indexes:
+            i = int(i)-1
+            i = list(filter(None, self.items_))[i]
+            i.fade = datetime.now()
+            i.expire = datetime.now()+timedelta(hours=24)
 
-    def expire(self, i):
-        self.items_[i].fade = datetime.now()-timedelta(hours=24)
-        self.items_[i].expire = datetime.now()
+    def expire(self, indexes):
+        for i in indexes:
+            i = int(i)-1
+            i = list(filter(None, self.items_))[i]
+            i.fade = datetime.now()-timedelta(hours=24)
+            i.expire = datetime.now()
 
-    def bump(self, i, items = None):
-        items = items or self.items_
-        i = list(filter(None,items))[i]
+    def bump(self, indexes, items_=None):
+        for i in indexes:
+            i = int(i)-1
+            items_ = items_ or self.items_
+            i = list(filter(None, items_))[i]
 
-        i.fade = datetime.now()+timedelta(hours=24)
-        i.expire = datetime.now()+timedelta(hours=72)
+            i.fade = datetime.now()+timedelta(hours=24)
+            i.expire = datetime.now()+timedelta(hours=72)
 
-    def empty_expired(self):
+    def empty_expired(self, arg):
         for item in items.expired():
             items.remove(item)
         print("emptied expired items")
 
-    def expired(self,i):
+    def expired(self, arg):
         it = items.expired()
+        self.skip_listing = 1
         if not it:
             print("No expired items")
             return
@@ -230,12 +294,18 @@ class ETodo:
             print("{0}) {1}".format(n+1,i))
 
         idx = input('bump> ').strip().split()
-        for i in idx:
-            self.bump(int(i)-1, items = it)
+        if not idx:
+            return
+        if idx[0]=='q':
+            self.quit()
+        try:
+            self.bump(idx, items_=it)
+        except Exception as e:
+            print("Error: %s"%e)
 
 
 if __name__=='__main__':
-    print("itemdb", itemdb)
+    # print("itemdb", itemdb)
     items = Items()
     etodo = ETodo()
     etodo.main()
